6942cf4458a54cab0f92dbee85c6d27ed420bf97
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-13 00:00:50 +0100

    feat: add command for copying file

---
+++ src/copy-file.js
@@ -0,0 +1,65 @@
+import { copyFile as copy, existsSync } from 'fs';
+import { resolve } from 'path';
+import { throwError } from './utils/throw-error.js';
+import { isString } from './utils/is-string.js';
+import { write } from './utils/write.js';
+
+export const copyFile = async ({
+    currentPath,
+    filename = '',
+    newDirectoryName = '',
+}) => {
+    const filePath = resolve(currentPath, filename);
+    const newDirectory = resolve(currentPath, newDirectoryName);
+
+    if (!isString(filePath)) {
+        throwError({
+            isOperationFailed: true,
+            error: { message: 'Pass correct file path' },
+        });
+        return;
+    }
+    if (!isString(newDirectory) || !existsSync(newDirectory)) {
+        throwError({
+            isOperationFailed: true,
+            error: {
+                message:
+                    'Incorrect directory name passed. Pass correct directory name',
+            },
+        });
+        return;
+    }
+
+    if (!existsSync(filePath)) {
+        throwError({
+            isOperationFailed: true,
+            error: {
+                message: `No file "${filename}" found by passed file path "${filePath}"`,
+            },
+        });
+        return;
+    }
+
+    const newFilePath = resolve(newDirectory, filename);
+
+    if (existsSync(newFilePath)) {
+        throwError({
+            isOperationFailed: true,
+            error: {
+                message: `File "${filename}" already exists in directory "${newDirectory}"`,
+            },
+        });
+        return;
+    }
+
+    copy(filePath, newFilePath, (copyErr) => {
+        if (copyErr) {
+            throwError({ isOperationFailed: true, error: copyErr });
+            return;
+        }
+
+        write(
+            `File "${filename}" was successfully copied to "${newDirectory}" folder`,
+        );
+    });
+};
