572da72d832077bebb546d588151663697142b3b
Varya Deviaterikova <hallovarvara@gmail.com>
2022-06-14 10:10:03 +0100

    fix: change ".stop" for ".exit"

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 import { homedir } from 'os';
 
-export const STOP_COMMAND = '.stop';
+export const STOP_COMMAND = '.exit';
 export const HOME_DIRECTORY = homedir();
 export const UNKNOWN_USERNAME = 'unforgettable Unknown';
